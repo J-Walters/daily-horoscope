ada68819b7215702e94245ac19f8afa8184ffa40
Jordan Walters <jwalters012@gmail.com>
2022-04-06 14:15:32 -0400

    basic homepage layout/css

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
 import { Player } from '@lottiefiles/react-lottie-player';
+import './Home.css';
 
 const Home = (props) => {
   return (
-    <div className='App'>
-      <header>
+    <div className='home-container'>
+      <header className='heading-container'>
         <h1>Daily Horoscope</h1>
         <h3>get everyday advice on how to navigate the planets</h3>
       </header>
-      <Player
-        autoplay
-        loop={true}
-        src='https://assets5.lottiefiles.com/packages/lf20_btkj8xsi.json'
-        style={{ height: '300px', width: '300px' }}
-      ></Player>
-      <Link to='/zodiac-signs'>
-        <button>Enter</button>
-      </Link>
+      <figure>
+        <Player
+          autoplay
+          loop={true}
+          src='https://assets5.lottiefiles.com/packages/lf20_btkj8xsi.json'
+          style={{ height: '300px', width: '300px' }}
+        ></Player>
+      </figure>
+      <div class='button-container'>
+        <Link to='/zodiac-signs'>
+          <button class='button'>Enter</button>
+        </Link>
+      </div>
     </div>
   );
 };
