3d6c30dd3edfde108b5801deba5e109caee17354
Jordan Walters <jwalters012@gmail.com>
2022-04-06 11:24:51 -0400

    link added to anchor

--- src/client/components/Zodiac.js
+++ src/client/components/Zodiac.js
@@ -19,9 +19,16 @@ const Zodiac = (props) => {
       <div>
         <h1>Choose Your Sun Sign</h1>
       </div>
-      {signs.map((sign) => (
-        <div>{sign}</div>
-      ))}
+      <div>
+        {signs.map((sign) => (
+          <h4>{sign}</h4>
+        ))}
+      </div>
+      <div>
+        <a href='https://en.wikipedia.org/wiki/Astrological_sign#Western_astrological_correspondence_chart'>
+          unsure about your sign?
+        </a>
+      </div>
     </div>
   );
 };
