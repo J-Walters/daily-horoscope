827b218564e2756ac012940c216f378f606137f3
Jordan Walters <jwalters012@gmail.com>
2022-04-05 22:03:40 -0400

    file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import './App.css';
-import { Player } from '@lottiefiles/react-lottie-player';
-import { Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from './client/components/Home';
 
 function App() {
   return (
-    <div className='App'>
-      <header>
-        <h1>Daily Horoscope</h1>
-        <h3>get everyday advice on how to navigate the planets</h3>
-      </header>
-      <Player
-        autoplay
-        loop={true}
-        src='https://assets5.lottiefiles.com/packages/lf20_btkj8xsi.json'
-        style={{ height: '300px', width: '300px' }}
-      ></Player>
-      <Link to='/horoscopes'>
-        <button>Enter</button>
-      </Link>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/client/components/Home.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { Player } from '@lottiefiles/react-lottie-player';
+
+const Home = (props) => {
+  return (
+    <div className='App'>
+      <header>
+        <h1>Daily Horoscope</h1>
+        <h3>get everyday advice on how to navigate the planets</h3>
+      </header>
+      <Player
+        autoplay
+        loop={true}
+        src='https://assets5.lottiefiles.com/packages/lf20_btkj8xsi.json'
+        style={{ height: '300px', width: '300px' }}
+      ></Player>
+      <Link to='/horoscopes'>
+        <button>Enter</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
