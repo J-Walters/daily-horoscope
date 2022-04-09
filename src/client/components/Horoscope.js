f51f4e01e82caaec89e4285a8ec6d1a580e2e23f
Jordan Walters <jwalters012@gmail.com>
2022-04-09 18:05:08 -0400

    horscope card inital style

--- src/client/components/Horoscope.js
+++ src/client/components/Horoscope.js
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Images from '../../horoscope-images/index';
+import './Horoscope.css';
+
 import axios from 'axios';
 import key from '../../Keys.js';
 
@@ -31,9 +34,29 @@ const Horoscope = () => {
   }, [params.sign]);
 
   return (
-    <div>
-      <h1>{params.sign}</h1>
-      <h1>{horoscope.description}</h1>
+    <div className='horoscope-container'>
+      <header className='header'>
+        <h1>{horoscope.current_date}</h1>
+      </header>
+      <div className='reading-container'>
+        <div className='zodiac-name'>
+          <h1>{params.sign}</h1>
+        </div>
+        <h1>{horoscope.date_range}</h1>
+        <img
+          src={Images[params.sign]}
+          alt={`The zodiac sign ${Images[params.sign]}`}
+        />
+        <div className='division'></div>
+        <h1 className='description'>{horoscope.description}</h1>
+        <div className='division'></div>
+        <h2>Compatibility</h2>
+        <h2>{horoscope.compatibility}</h2>
+        <h2>Lucky Number</h2>
+        <h2>{horoscope.lucky_number}</h2>
+        <h2>Mood</h2>
+        <h2>{horoscope.mood}</h2>
+      </div>
     </div>
   );
 };
